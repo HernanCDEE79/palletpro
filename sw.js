96bade98a506daaf4e9350aecf92a72c4409435d
HernanCDEE79 <hisepro.hlopez@gmail.com>
2026-02-19 17:45:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_VERSION = 'palletpro-v25';
+
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then(cache => {
+      return cache.addAll(ASSETS).catch(err => console.warn('[SW] Cache parcial:', err));
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+     .then(() => self.clients.matchAll({ type: 'window' }).then(clients =>
+       clients.forEach(c => c.postMessage({ type: 'SW_UPDATED', version: CACHE_VERSION }))
+     ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+
+  if (url.pathname.endsWith('index.html') || url.pathname === '/' || url.pathname.endsWith('/')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          caches.open(CACHE_VERSION).then(cache => cache.put(event.request, response.clone()));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        caches.open(CACHE_VERSION).then(cache => cache.put(event.request, response.clone()));
+        return response;
+      })
+    )
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
